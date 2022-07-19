2b1c21d259340aa6b36ebf894e012fed3575ebf6
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-07-20 00:52:55 +0200

    Update von

---
+++ deploylocal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#team17_erfkarimi
+#2022-05-18_16:29:55
+#Thema: 
+
+
+mkdir -p /var/www/html/$USER/swe2_2022team17/
+
+
+mkdir -p www
+cd www
+
+cp -r . /var/www/html/$USER/swe2_2022team17/
+
+echo "Link: https://informatik.hs-bremerhaven.de/$USER/swe2_2022team17/"
